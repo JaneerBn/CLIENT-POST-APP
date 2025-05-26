130f409a647df0497fc261dc4d04d22cd7091aa1
Janeer Barrera <barrera.janeer@gmail.com>
2025-05-25 19:00:49 -0500

    Correcciones

--- src/app/front-page/components/footer-front/footer-front.component.ts
+++ src/app/front-page/components/footer-front/footer-front.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer-front',
+  selector: 'footer-front',
   imports: [],
   templateUrl: './footer-front.component.html',
   styleUrl: './footer-front.component.css'
 })
 export class FooterFrontComponent {
-
 }
