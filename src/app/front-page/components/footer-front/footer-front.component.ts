6fd540dcfc371a4f9cb9110910d6121a45802a14
Janeer Barrera <barrera.janeer@gmail.com>
2025-05-25 22:07:32 -0500

    correcciones

--- src/app/front-page/components/footer-front/footer-front.component.ts
+++ src/app/front-page/components/footer-front/footer-front.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'footer-front',
   imports: [],
   templateUrl: './footer-front.component.html',
-  styleUrl: './footer-front.component.css'
+  styleUrl: './footer-front.component.css',
 })
 export class FooterFrontComponent {
+  currentYear = signal(new Date().getFullYear());
 }
--- src/app/front-page/front.routes.ts
+++ src/app/front-page/front.routes.ts
@@ -1,20 +1,21 @@
-import { Routes } from "@angular/router";   
-import { FrontLayoutComponent } from "./layouts/front-layout/front-layout.component";
-import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { Routes } from '@angular/router';
+import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
 const frontRoutes: Routes = [
-    {
+  {
+    path: '',
+    component: FrontLayoutComponent,
+    children: [
+      {
         path: '',
-        component: FrontLayoutComponent,
-        children: [
-            {
-                path: '',
-                component: HomePageComponent
-            },
-            {
-                path: '',
-                loadComponent:()=>import('./pages/not-found-page/not-found-page.component'),
-            }
-        ]
-    },
-]
+        component: HomePageComponent,
+      },
+      {
+        path:'**',
+        loadComponent:()=> import('./pages/not-found-page/not-found-page.component')
+      }
+    ],
+  },
+];
 export default frontRoutes;
