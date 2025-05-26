5ba3b75e9530e46dc9f319ff81ed19121cad9299
Janeer Barrera <barrera.janeer@gmail.com>
2025-05-25 21:50:14 -0500

    auth folder

---
+++ src/app/auth/guards/authenticated.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanMatchFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { firstValueFrom } from 'rxjs';
+
+export const authenticatedGuard: CanMatchFn = async (route, segments) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const isAutheticated = await firstValueFrom(authService.checkStatus());
+
+  if (!isAutheticated) {
+    router.navigateByUrl('/autg/login');
+    return false;
+  }
+
+
+  return true;
+};
